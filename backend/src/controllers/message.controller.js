aafbbb641e52c2b5521917cf2ce1bf93563665e0
Samuel <109113326+samuxka@users.noreply.github.com>
2025-01-18 19:06:17 -0300

    end backend

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -19,15 +19,45 @@ export const getMessages = async (req, res) => {
         const myId = req.user._id
 
         const messages = await Message.find({
-            $or:[
-                {senderId:myId, receiverId:userToChatId},
-                {senderId:userToChatId, receiverId:myId}
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId }
             ]
         })
 
         res.status(200).json(messages)
     } catch (error) {
         console.log("Error in getMessages controller: ", error.message)
-        res.status(500).json({error:"Internal server error"})
+        res.status(500).json({ error: "Internal server error" })
+    }
+}
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body
+        const { id: receiverId } = req.params
+        const senderId = req.user._id
+
+        let imageUrl
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            imagem: imageUrl
+        })
+
+        await newMessage.save()
+
+
+        res.status(201).json(newMessage)
+
+    } catch (error) {
+        console.log("Error in sendMessage controller: ", error.message)
+        res.status(500).json({ error: "Internal server error" })
     }
 }
